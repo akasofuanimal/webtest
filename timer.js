71a47dd47d96bcb050a6c9b27be048d05177bd46
akasofuanimal <64628138+akasofuanimal@users.noreply.github.com>
2024-01-12 10:10:10 +0900

    Update timer.js

--- timer.js
+++ timer.js
@@ -5,21 +5,22 @@
     //     () => console.log('許可されました！'),
     //     () => console.log('許可されませんでした。')
     // );
-
-    $('#allow_push_notification').click(function () {
+    var allow_notification = document.getElementById('allow_push_notification');
+   
+    allow_notification.addEventListener('click', function () {
     // ブラウザが通知をサポートしているか確認する
       // 許可を求める
       Notification.requestPermission()
         .then((permission) => {
-        if (permission == 'granted') {
-        // 許可
-        } else if (permission == 'denied') {
-        // 拒否
-          alert("拒否");
-        } else if (permission == 'default') {
-        // 無視
-          alert("無視");
-        }
+           if (permission == 'granted') {
+           // 許可
+           } else if (permission == 'denied') {
+           // 拒否
+             alert("拒否");
+           } else if (permission == 'default') {
+           // 無視
+             alert("無視");
+           }
       });
     });
 })();
